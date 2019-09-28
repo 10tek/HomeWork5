601dccad1329b2a22e290a398b78468524a7f797
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-09-28 18:04:49 +0600

    Сражение танков с помощью класса из библиоетки классов.

---
+++ Day7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib.WorldOfTanks;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // При разработке программы использовать обработку исключительных ситуаций. Варианты возможных исключительных ситуаций рассмотреть самостоятельно!
+            // Не смог найти варианты возможных искл. ситуаций.
+            var tanksT34 = new Tank[5];
+            var tanksPantera = new Tank[5];
+            var winner = 0;
+            for (var i = 0; i < tanksT34.Length; i++)
+            {
+                tanksT34[i] = new Tank("T-34_" + i);
+                tanksPantera[i] = new Tank("Pantera_" + i);
+            }
+            Console.WriteLine("Team T-34:");
+            foreach (var tank in tanksT34)
+            {
+                Console.WriteLine(tank.GetStats());
+            }
+            Console.WriteLine("Team Pantera:");
+            foreach (var tank in tanksPantera)
+            {
+                Console.WriteLine(tank.GetStats());
+            }
+            for (var i = 0; i < tanksPantera.Length; i++)
+            {
+                if (tanksT34[i] * tanksPantera[i])
+                {
+                    winner++;
+                }
+                else
+                {
+                    winner--;
+                }
+            }
+            if (winner > 0)
+            {
+                Console.WriteLine("Победила команда T-34");
+            }
+            else
+            {
+                Console.WriteLine("Победила команда Pantera");
+            }
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyClassLib.WorldOfTanks
+{
+    public class Tank
+    {
+        private string _name;
+        private int _attack;
+        private int _armor;
+        private int _mobility;
+        private static Random random = new Random();
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public int Attack
+        {
+            get
+            {
+                return _attack;
+            }
+        }
+
+        public int Armor
+        {
+            get
+            {
+                return _armor;
+            }
+        }
+
+        public int Mobility
+        {
+            get
+            {
+                return _mobility;
+            }
+        }
+
+        public Tank()
+        {
+            _name = string.Empty;
+            _attack = random.Next(0, 100);
+            _armor = random.Next(0, 100);
+            _mobility = random.Next(0, 100);
+        }
+
+        public Tank(string name)
+        {
+            _name = name;
+            _attack = random.Next(0, 100);
+            _armor = random.Next(0, 100);
+            _mobility = random.Next(0, 100);
+        }
+
+        public string GetStats()
+        {
+            return $"name: {Name}; attack: {_attack}; armor: {_armor}; mobility: {_mobility}";
+        }
+
+        public static bool operator *(Tank firstTank, Tank secondTank)
+        {
+            int winner = 0;
+            Console.WriteLine($"Name:   {firstTank.Name} vs {secondTank.Name}");
+            Console.WriteLine($"Attack:   {firstTank.Attack} - {secondTank.Attack}");
+            Console.WriteLine($"Armor:    {firstTank.Armor} - {secondTank.Armor}");
+            Console.Write($"Mobility: {firstTank.Mobility} - {secondTank.Mobility}");
+            winner += firstTank.Attack > secondTank.Attack ? 1 : -1;
+            winner += firstTank.Armor > secondTank.Armor ? 1 : -1;
+            winner += firstTank.Mobility > secondTank.Mobility ? 1 : -1;
+            if (winner > 0)
+            {
+                Console.WriteLine($"  {firstTank.Name} wins\n");
+                return true;
+            }
+            Console.WriteLine($"  {secondTank.Name} wins\n");
+            return false;
+        }
+    }
+}
